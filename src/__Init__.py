c72d1860fe00924473591e8676669316566778fa
MattimoreKelly <122730499+MattimoreKelly@users.noreply.github.com>
2025-03-18 14:12:25 +0000

    Create __Init__.py

---
+++ src/__Init__.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2025 MKM Research Labs. All rights reserved.
+# 
+# This software is provided under license by MKM Research Labs. 
+# Use, reproduction, distribution, or modification of this code is subject to the 
+# terms and conditions of the license agreement provided with this software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
